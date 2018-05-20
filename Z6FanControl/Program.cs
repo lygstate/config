f35d6ddfd1bf3fe3b73504dc8fb08056429cb169
Yonggang Luo <luoyonggang@gmail.com>
2018-05-20 23:47:37 +0800

    管理员模式运行

--- Z6FanControl/Program.cs
+++ Z6FanControl/Program.cs
@@ -62,37 +62,140 @@ namespace Z6FanControl
         }
     }
 
+    public static class MySetting
+    {
+        public static void WinKeyOnOff()
+        {
+            object data = (object)0;
+            WMIEC.WMIReadECRAM(1895UL, ref data);
+            WMIEC.WMIWriteECRAM(1895UL, 1UL + (Convert.ToUInt64(data) & 254UL));
+        }
+
+        public static void WinKeyOn()
+        {
+            if (((long)GetAddrStatusByte() & 1L) == 1L)
+            {
+                Utility.ComputerMonitorLog("WinKey ON");
+                WinKeyOnOff();
+            }
+        }
+
+        public static void WinKeyOff()
+        {
+            if (((long)GetAddrStatusByte() & 1L) == 0L)
+            {
+                Utility.ComputerMonitorLog("WinKey Off");
+                WinKeyOnOff();
+            }
+        }
+
+        public static void LightBarOnOff()
+        {
+            object data = (object)0;
+            WMIEC.WMIReadECRAM(1895UL, ref data);
+            WMIEC.WMIWriteECRAM(1895UL, 2UL + (Convert.ToUInt64(data) & 253UL));
+        }
+
+        public static void SilentModeOnOff()
+        {
+            object data = (object)0;
+            WMIEC.WMIReadECRAM(1895UL, ref data);
+            WMIEC.WMIWriteECRAM(1895UL, 8UL + (Convert.ToUInt64(data) & 247UL));
+        }
+
+        public static void USBChargeOn()
+        {
+            object data = (object)0;
+            WMIEC.WMIReadECRAM(1895UL, ref data);
+            WMIEC.WMIWriteECRAM(1895UL, 16UL + (Convert.ToUInt64(data) & 239UL));
+        }
+
+        public static void USBChargeOff()
+        {
+            object data = (object)0;
+            WMIEC.WMIReadECRAM(1895UL, ref data);
+            WMIEC.WMIWriteECRAM(1895UL, Convert.ToUInt64(data) & 239UL);
+        }
+
+        public static void TouchPadOnOff()
+        {
+            object data = (object)0;
+            WMIEC.WMIReadECRAM(1885UL, ref data);
+            WMIEC.WMIWriteECRAM(1885UL, 4UL + (Convert.ToUInt64(data) & 251UL));
+        }
+
+        public static ulong GetAddrStatusByte()
+        {
+            object Buffer = (object)0; ;
+            WMIEC.WMIReadECRAM(1896UL, ref Buffer);
+            return Convert.ToUInt64(Buffer);
+        }
+    }
 
     class Program
     {
 
-        public static void SetFanLevel(uint level)
+        /// <summary>
+        /// 设置灯光以及节能模式
+        /// </summary>
+        /// <param name="isLightBarOn">是否打开灯光条</param>
+        /// <param name="isPowerSaveMode">是否打开节能模式</param>
+        /// <param name="isKeyPress">TODO: isKeyPress</param>
+        /// <param name="isNoKey">TODO:isNoKey</param>
+        /// <returns></returns>
+        public static void WriteEC0748Byte(bool isLightBarOn, bool isPowerSaveMode, bool isKeyPress, bool isNoKey)
+        {
+            byte num = 1;
+            if (isLightBarOn)
+            {
+                Utility.Log(string.Format("[WriteEC0748Byte] AP RGBLBStatus ON"));
+                num &= (byte)251;
+            }
+            else
+            {
+                Utility.Log("[WriteEC0748Byte] AP RGBLBStatus OFF");
+                num |= (byte)4;
+            }
+            if (isPowerSaveMode)
+            {
+                Utility.Log("[WriteEC0748Byte] AP PowerSaveMode ON");
+                num |= (byte)2;
+            }
+            else
+            {
+                Utility.Log("[WriteEC0748Byte] AP PowerSaveMode OFF");
+                num &= (byte)253;
+            }
+            if (isKeyPress)
+            {
+                Utility.Log("[WriteEC0748Byte] AP isKeyPress set 0x0748H.5 = 1 ");
+                num |= (byte)32;
+            }
+            if (isNoKey)
+            {
+                Utility.Log("[WriteEC0748Byte] AP isNoKey set 0x0748H.4 = 1 ");
+                num |= (byte)16;
+            }
+            WMIEC.WMIWriteECRAM(1864UL, (ulong)num);
+            Utility.Log(string.Format("[WriteEC0748Byte] AP set 0x0748h = 0x{0:X}", (object)num));
+        }
+
+        /// <summary>
+        /// 设置风扇速度,手动模式
+        /// </summary>
+        /// <param name="level">风扇速度级别，范围[0,7]，0表示无风</param>
+        /// <returns></returns>
+        public static void SetFanSpeedManualLevel(uint level)
         {
             WMIEC.WMIWriteECRAM(1873UL, 128UL + (ulong)level);
         }
-        static private ulong PWM2Byte(uint level)
+
+        static void SetFanSpeedForTemperatureLevel(uint level, uint speed)
         {
-            ulong num = 0;
-            switch (level)
-            {
-                case 1:
-                    num = 60UL;
-                    break;
-                case 2:
-                    num = 80UL;
-                    break;
-                case 3:
-                    num = 100UL;
-                    break;
-                case 4:
-                    num = 120UL;
-                    break;
-                case 5:
-                    num = 140UL;
-                    break;
-            }
-            return num;
+
+            WMIEC.WMIWriteECRAM(1857UL + level, speed);
         }
+
 
         /// <summary>
         /// 设置风扇速度
@@ -103,64 +206,143 @@ namespace Z6FanControl
         {
             /* L0 不知道是否存在 */
             /* L0 40 degree */
-            WMIEC.WMIWriteECRAM(1857UL, speed);
+            SetFanSpeedForTemperatureLevel(0U, speed);
 
             /* L1 45 degree */
-            WMIEC.WMIWriteECRAM(1858UL, speed);
+            SetFanSpeedForTemperatureLevel(1U, speed);
 
             /* L2 50 degree */
-            WMIEC.WMIWriteECRAM(1859UL, speed);
+            SetFanSpeedForTemperatureLevel(2U, speed);
 
             /* L3 55 degree */
-            WMIEC.WMIWriteECRAM(1860UL, speed);
+            SetFanSpeedForTemperatureLevel(3U, speed);
 
             /* L4 60 degree */
-            WMIEC.WMIWriteECRAM(1861UL, speed);
+            SetFanSpeedForTemperatureLevel(4U, speed + 25);
 
             /* L5 65 degree */
-            WMIEC.WMIWriteECRAM(1862UL, speed);
+            SetFanSpeedForTemperatureLevel(5U, speed + 55);
 
-            if (false) {
-                /* 不确定是否支持 */
-                /* L6 70 degree */
-                WMIEC.WMIWriteECRAM(1863UL, speed);
+            /* 最高支持70度 */
+            /* L6 70 degree */
+            SetFanSpeedForTemperatureLevel(6U, speed + 55);
+        }
 
-                /* L7 75 degree */
-                WMIEC.WMIWriteECRAM(1864UL, speed);
+        /// <summary>
+        /// 设置状态栏颜色
+        /// </summary>
+        /// <param name="red_level">风扇速度，范围 [0,36]</param>
+        /// <param name="green_level">风扇速度，范围 [0,36]</param>
+        /// <param name="blue_level">风扇速度，范围 [0,36]</param>
+        /// <returns></returns>
+        public static void SetLightBarColor(uint red_level, uint green_level, uint blue_level)
+        {
+            Utility.MyLightBarLog(string.Format("red 0x0749h = {0}", (object)red_level));
+            Utility.MyLightBarLog(string.Format("green 0x074Ah = {0}", (object)green_level));
+            Utility.MyLightBarLog(string.Format("blue 0x074Bh = {0}", (object)blue_level));
+            WMIEC.WMIWriteECRAM(1865UL, red_level);
+            WMIEC.WMIWriteECRAM(1866UL, green_level);
+            WMIEC.WMIWriteECRAM(1867UL, blue_level);
+        }
 
-                /* L8 80 degree */
-                WMIEC.WMIWriteECRAM(1865UL, speed);
+        private static void Write_Support_BYTE()
+        {
+            object data = (object)0;
+            WMIEC.WMIReadECRAM(1894UL, ref data);
+            WMIEC.WMIWriteECRAM(1894UL, Convert.ToUInt64(data) | 3UL);
+        }
+
+
+        public static void InitKeyboard()
+        {
+            object data = (object)0;
+            WMIEC.WMIReadECRAM(1857UL, ref data);
+            WMIEC.WMIWriteECRAM(1857UL, (ulong)(byte)(Convert.ToUInt64(data) & 254UL));
+        }
+
+        public static void KeyboardSet_APExistToEC()
+        {
+            object data = (object)0;
+            WMIEC.WMIReadECRAM(1857UL, ref data);
+            WMIEC.WMIWriteECRAM(1857UL, (ulong)(byte)((uint)(byte)Convert.ToUInt64(data) | 1U));
+        }
+
+        private static void KeyboardEnable_EC_OnkeyPressed()
+        {
+            object data = (object)0;
+            WMIEC.WMIReadECRAM(1857UL, ref data);
+            Utility.MyLightBarLog(string.Format("Enable_EC_OnkeyPressed data {0}", (object)data));
+            WMIEC.WMIWriteECRAM(1857UL, (ulong)(byte)((uint)(byte)Convert.ToUInt64(data) | 8U));
+        }
+
+        private static void InitSilentMode()
+        {
+            object data = (object)0;
+            WMIEC.WMIReadECRAM(1115UL, ref data);
+            if (((long)Convert.ToUInt64(data) & 1L) == 1L)
+            {
+                object data2 = (object)0;
+                WMIEC.WMIReadECRAM(1895UL, ref data2);
+                WMIEC.WMIWriteECRAM(1895UL, 8UL + (Convert.ToUInt64(data2) & 247UL));
+                Utility.Log("[InitSilentMode] Works");
             }
+            else
+            {
+                Utility.Log("[InitSilentMode] Have no SilentMode");
+            }
+            Utility.Log("[InitSilentMode] End");
+        }
 
-
-            /* 自定义不同温度的风扇速度，现在是将所有风扇速度设置为一样 */
-            WMIEC.WMIWriteECRAM(1873UL, 160UL);
+        private static void InitUSBCharger()
+        {
+            object data = (object)0;
+            WMIEC.WMIReadECRAM(1895UL, ref data);
+            WMIEC.WMIWriteECRAM(1895UL, Convert.ToUInt64(data) & 239UL);
+            Utility.Log("[Init USB Charger] End");
         }
 
         static void Main(string[] args)
         {
-            while (true) {
+            Write_Support_BYTE();
+
+            InitKeyboard();
+
+            InitSilentMode();
+
+            InitUSBCharger();
+
+            MySetting.USBChargeOn();
+            MySetting.WinKeyOn();
+            // KeyboardSet_APExistToEC();
+
+            // KeyboardEnable_EC_OnkeyPressed();
+
+            WriteEC0748Byte(false, true, true, false);
+            MySetting.LightBarOnOff();
+
+            SetLightBarColor(10, 10, 10);
+
+            /* 转速20是最大的没有音量的声音，适合办公或者看电影，比较安静，深夜办公 */
+            SetFanSpeed(20);
+
+            /* 转速40是最大的没有音量的声音，适合办公或者看电影，比较安静，白天办公 */
+            // SetFanSpeed(40);
+
+            /* 转速45是最大的没有音量的声音，适合办公或者看电影，比较安静，白天办公 */
+            // SetFanSpeed(45);
+
+            while (true)
+            {
                 /* 全速运行 */
                 // WMIEC.WMIWriteECRAM(1873UL, 64UL);
 
                 /* 智能能模式 */
                 // WMIEC.WMIWriteECRAM(1873UL, 0UL);
 
-                if (false) { 
-                    /* 无风 */
-                    uint level = 7; /* 0 无风， 1-7，不同档次风 */
-                    SetFanLevel(level);
-                    Thread.Sleep(3000);
-                    SetFanLevel(0);
-                }
-
-                /* 转速35是最大的没有音量的声音，适合办公或者看电影，比较安静，深夜办公 */
-                SetFanSpeed(35);
-
-                /* 转速45是最大的没有音量的声音，适合办公或者看电影，比较安静，白天办公 */
-                // SetFanSpeed(45);
-
+                /* 将风扇模式设置为 自定义不同温度的风扇速度 */
+                WMIEC.WMIWriteECRAM(1873UL, 160UL);
                 Thread.Sleep(500);
+                break;
             }
         }
     }
---
+++ Z6FanControl/Utility.cs
@@ -0,0 +1,224 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: GamingCenter.Utility
+// Assembly: GamingCenter, Version=1.0.0.33, Culture=neutral, PublicKeyToken=null
+// MVID: AC6C2AAD-0313-4B01-B972-649C5F39DAED
+// Assembly location: C:\Program Files\OEM\GamingCenter\GamingCenter.exe
+
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace Z6FanControl
+{
+  public static class Utility
+  {
+    private static bool b_LogEnable;
+
+    public static object RegistrySoftwareKeyRead(RegistryHive hKey, string SubKey, string Name, object defaultvalue)
+    {
+      if (Environment.Is64BitOperatingSystem)
+        return RegistryKey.OpenBaseKey(hKey, RegistryView.Registry64).OpenSubKey("SOFTWARE" + SubKey).GetValue(Name, defaultvalue);
+      return RegistryKey.OpenBaseKey(hKey, RegistryView.Registry32).OpenSubKey("SOFTWARE\\WOW6432Node" + SubKey).GetValue(Name, defaultvalue);
+    }
+
+    public static void RegistrySoftwareKeyWrite(RegistryHive hKey, string SubKey, string Name, object setvalue, RegistryValueKind valuekind)
+    {
+      if (Environment.Is64BitOperatingSystem)
+        RegistryKey.OpenBaseKey(hKey, RegistryView.Registry64).CreateSubKey("SOFTWARE" + SubKey).SetValue(Name, setvalue, valuekind);
+      else
+        RegistryKey.OpenBaseKey(hKey, RegistryView.Registry32).CreateSubKey("SOFTWARE\\WOW6432Node" + SubKey).SetValue(Name, setvalue, valuekind);
+    }
+
+    public static void RegistrySoftwareKeyDelete(RegistryHive hKey, string SubKey, string Name)
+    {
+      if (Environment.Is64BitOperatingSystem)
+        RegistryKey.OpenBaseKey(hKey, RegistryView.Registry64).CreateSubKey("SOFTWARE" + SubKey).DeleteValue(Name);
+      else
+        RegistryKey.OpenBaseKey(hKey, RegistryView.Registry32).CreateSubKey("SOFTWARE\\WOW6432Node" + SubKey).DeleteValue(Name);
+    }
+
+    public static object RegistryKeyRead(RegistryHive hKey, string SubKey, string Name, object defaultvalue)
+    {
+      if (Environment.Is64BitProcess)
+        return RegistryKey.OpenBaseKey(hKey, RegistryView.Registry64).OpenSubKey(SubKey).GetValue(Name, defaultvalue);
+      return RegistryKey.OpenBaseKey(hKey, RegistryView.Registry32).OpenSubKey(SubKey).GetValue(Name, defaultvalue);
+    }
+
+    public static void RegistryKeyWrite(RegistryHive hKey, string SubKey, string Name, object setvalue, RegistryValueKind valuekind)
+    {
+      if (Environment.Is64BitProcess)
+        RegistryKey.OpenBaseKey(hKey, RegistryView.Registry64).CreateSubKey(SubKey).SetValue(Name, setvalue, valuekind);
+      else
+        RegistryKey.OpenBaseKey(hKey, RegistryView.Registry32).CreateSubKey(SubKey).SetValue(Name, setvalue, valuekind);
+    }
+
+    public static void CheckProcessExistAndCreate(string processeName, string path)
+    {
+      bool flag = false;
+      try
+      {
+        foreach (Process process in Process.GetProcessesByName(processeName))
+          flag = true;
+      }
+      catch
+      {
+      }
+      if (flag)
+        return;
+      try
+      {
+        Process.Start(new ProcessStartInfo()
+        {
+          FileName = processeName,
+          WorkingDirectory = path,
+          UseShellExecute = true,
+          CreateNoWindow = false,
+          RedirectStandardInput = false,
+          RedirectStandardOutput = false
+        });
+        Thread.Sleep(1000);
+      }
+      catch
+      {
+        Console.WriteLine("CheckProcessExistAndCreate failed");
+      }
+    }
+
+    public static void EnableLog()
+    {
+      try
+      {
+        Utility.b_LogEnable = (int) Utility.RegistryKeyRead(RegistryHive.LocalMachine, "SOFTWARE\\OEM\\GamingCenterService\\Debug", "enable", (object) 0) > 0;
+      }
+      catch
+      {
+        Utility.b_LogEnable = false;
+        return;
+      }
+      try
+      {
+        if ((int) Utility.RegistryKeyRead(RegistryHive.LocalMachine, "SOFTWARE\\OEM\\GamingCenterService\\Debug", "create", (object) 0) <= 0)
+          return;
+        File.Delete("C:\\GamingCenter.log");
+        File.Delete("C:\\GamingCenterFanManagement.log");
+      }
+      catch
+      {
+      }
+    }
+
+    public static void DisableLog()
+    {
+      Utility.b_LogEnable = false;
+    }
+
+    public static void Log(string logMessage)
+    {
+      try
+      {
+        if (Utility.b_LogEnable)
+        {
+          using (StreamWriter streamWriter = File.AppendText("C:\\GamingCenter.log"))
+            streamWriter.WriteLine("{0}", (object) logMessage);
+        }
+        else
+          Console.WriteLine(logMessage);
+      }
+      catch
+      {
+      }
+    }
+
+    public static void ComputerMonitorLog(string logMessage)
+    {
+      try
+      {
+        if (Utility.b_LogEnable)
+        {
+          using (StreamWriter streamWriter = File.AppendText("C:\\GamingCenterComputerMonitor.log"))
+            streamWriter.WriteLine("{0}", (object) logMessage);
+        }
+        else
+          Console.WriteLine(logMessage);
+      }
+      catch
+      {
+      }
+    }
+
+    public static void MyLightBarLog(string logMessage)
+    {
+      try
+      {
+        if (Utility.b_LogEnable)
+        {
+          using (StreamWriter streamWriter = File.AppendText("C:\\GamingCenterMyLightBarLog.log"))
+            streamWriter.WriteLine("{0}", (object) logMessage);
+        }
+        else
+          Console.WriteLine(logMessage);
+      }
+      catch
+      {
+      }
+    }
+
+    public static void FanManagementLog(string logMessage)
+    {
+      try
+      {
+        if (Utility.b_LogEnable)
+        {
+          using (StreamWriter streamWriter = File.AppendText("C:\\GamingCenterFanManagement.log"))
+            streamWriter.WriteLine("{0}", (object) logMessage);
+        }
+        else
+          Console.WriteLine(logMessage);
+      }
+      catch
+      {
+      }
+    }
+
+    public static void FastSwitchLog(string logMessage)
+    {
+      try
+      {
+        if (Utility.b_LogEnable)
+        {
+          using (StreamWriter streamWriter = File.AppendText("C:\\GamingCenterFastSwitch.log"))
+            streamWriter.WriteLine("{0}", (object) logMessage);
+        }
+        else
+          Console.WriteLine(logMessage);
+      }
+      catch
+      {
+      }
+    }
+
+    public static void CustomizeLog(string logMessage)
+    {
+      try
+      {
+        if (Utility.b_LogEnable)
+        {
+          using (StreamWriter streamWriter = File.AppendText("C:\\GamingCenterCustomizeLog.log"))
+            streamWriter.WriteLine("{0}", (object) logMessage);
+        }
+        else
+          Console.WriteLine(logMessage);
+      }
+      catch
+      {
+      }
+    }
+
+    public static string GetTime()
+    {
+      return DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss_fff");
+    }
+  }
+}
