be9f6231c4f0abcc06af9fdf22057be58a63aad5
Yonggang Luo <luoyonggang@gmail.com>
2021-05-12 21:20:52 +0800

    Fixes addTools-ARM.bat

--- qt/addTools.bat
+++ qt/addTools.bat
@@ -6,40 +6,6 @@ set QTROOT=%CD%
 cd /d %QTROOT%\qtcreator-4.14.2\bin
 :: sdktool https://github.com/qt-creator/qt-creator/tree/master/src/tools/sdktool
 
-set ARM_LINUX_GNUEABIHF_4_9_2_ROOT=%ROOT%\MentorGraphics\linaro
-sdktool addTC --id "ProjectExplorer.ToolChain.Gcc:arm-linux-gnueabihf-4.9.2-C" ^
-    --language 1 ^
-    --name "GCC arm-linux-gnueabihf-4.9.2 (C)" ^
-    --path "%ARM_LINUX_GNUEABIHF_4_9_2_ROOT%\bin\arm-linux-gnueabihf-gcc.exe" ^
-    --abi "arm-linux-generic-elf-32" ^
-    --supportedAbis arm-linux-generic-elf-32
-sdktool addTC --id "ProjectExplorer.ToolChain.Gcc:arm-linux-gnueabihf-4.9.2-CPP" ^
-    --language 2 ^
-    --name "GCC arm-linux-gnueabihf-4.9.2 (CPP)" ^
-    --path "%ARM_LINUX_GNUEABIHF_4_9_2_ROOT%\bin\arm-linux-gnueabihf-g++.exe" ^
-    --abi "arm-linux-generic-elf-32" ^
-    --supportedAbis arm-linux-generic-elf-32
-sdktool addDebugger ^
-    --id "company.product.toolchain.gdb.arm-linux-gnueabihf-4.9.2" ^
-    --name "GCC arm-linux-gnueabihf-4.9.2 (GDB)" ^
-    --engine 1 ^
-    --binary "%ARM_LINUX_GNUEABIHF_4_9_2_ROOT%\bin\arm-linux-gnueabihf-gdb.exe" ^
-    --abis arm-linux-generic-elf-32
-sdktool addQt --id "company.product.qt.arm-linux-gnueabihf-4.9.2" ^
-    --name "Qt %%{Qt:Version} arm-linux-gnueabihf-4.9.2" ^
-    --qmake %ARM_LINUX_GNUEABIHF_4_9_2_ROOT%\arm-linux-gnueabihf\libc\usr\local\qt5\bin\qmake.exe ^
-    --type RemoteLinux.EmbeddedLinuxQt
-sdktool addKit ^
-    --id "company.product.kit.qt-arm-linux-gnueabihf-4.9.2" ^
-    --name "EmbeddedLinux Qt %%{Qt:Version} arm-linux-gnueabihf-4.9.2" ^
-    --devicetype RemoteLinux.EmbeddedLinuxQt ^
-    --qt "company.product.qt.arm-linux-gnueabihf-4.9.2" ^
-    --debuggerid "company.product.toolchain.gdb.arm-linux-gnueabihf-4.9.2" ^
-    --qt "company.product.qt.arm-linux-gnueabihf-4.9.2" ^
-    --sysroot "%ARM_LINUX_GNUEABIHF_4_9_2_ROOT%\arm-linux-gnueabihf\libc" ^
-    --Ctoolchain "ProjectExplorer.ToolChain.Gcc:arm-linux-gnueabihf-4.9.2-C" ^
-    --Cxxtoolchain "ProjectExplorer.ToolChain.Gcc:arm-linux-gnueabihf-4.9.2-CPP"
-
 sdktool addQt ^
     --id "company.product.qt5.1.1.msvc2010_opengl_32bit" ^
     --name "Qt %%{Qt:Version} MSVC2010 OpenGL 32bit" ^
